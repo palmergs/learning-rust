d59320550a0d041fbb45a84cdc960a3e6eae01f1
Galen Palmer <palmergs@gmail.com>
2015-08-31 21:33:03 -0400

    working with colectors

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,3 +1,22 @@
+
+
+fn collector() {
+    println!("Build range into vector");
+    let one_to_ten = (1..10).collect::<Vec<_>>();
+    for num in &one_to_ten {
+        println!("{}", num);  
+    }
+}
+
+fn itvector() {
+    println!("Iterate over vector");
+    let nums = vec![1, 2, 3];
+    for num in &nums {
+        println!("{}", num);  // num is pointer to i32
+        println!("{}", *num); // dereference num
+    }
+}
+
 fn main() {
     let mut range = 0..10;
     loop {
@@ -8,5 +27,7 @@ fn main() {
             None => { break }
         }
     }
+    itvector();
+    collector();
     println!("Hello, world!");
 }
