b1428fb89880f93a4d238af381c32167a04ac980
Galen Palmer <palmergs@gmail.com>
2015-09-24 22:02:20 -0400

    initial try of ffi to snappy library

---
+++ ffi/src/main.rs
@@ -0,0 +1,25 @@
+extern crate libc;
+use libc::{ c_int, size_t };
+
+#[link(name = "snappy")]
+extern {
+    fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+    fn snappy_compress(input: *const u8,
+            input_length: size_t,
+            compressed: *mut u8,
+            compressed_length: *mut size_t) -> c_int;
+    fn snappy_uncompress(compressed: *const u8,
+            compressed_length: size_t,
+            uncompressed: *mut u8,
+            uncompressed_length: *mut size_t) -> c_int;
+    fn snappy_uncompressed_length(compressed: *const u8,
+            compressed_length: size_t,
+            result: *mut size_t) -> c_int;
+    fn snappy_validate_compressed_buffer(compressed: *const u8,
+            compressed_length: size_t) -> c_int;
+}
+
+fn main() {
+    let x = unsafe { snappy_max_compressed_length(100) };
+    println!("max compressed length of a 100 byte buffer: {}", x);
+}
