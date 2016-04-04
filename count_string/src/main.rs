b55e4d4ae5fc25d427d57531bc6aa249ad6f41ae
Galen Palmer <palmergs@gmail.com>
2016-04-04 10:15:44 -0400

    read from file into string with match on count

--- count_string/src/main.rs
+++ count_string/src/main.rs
@@ -4,11 +4,26 @@ use std::io;
 
 fn main() {
     let input = env::args().nth(1).unwrap();
-    let mut reader: Box<io::Read> = if input == "-" {
-        Box::new(io::stdin())
-    } else {
-        Box::new(fs::File::open(input).unwrap())
+
+    //let mut reader: Box<io::Read> = if input == "-" {
+    //    Box::new(io::stdin())
+    //} else {
+    //    Box::new(fs::File::open(input).unwrap())
+    //};
+
+    let mut reader: Box<io::Read> = match input.as_ref() {
+        "-" => Box::new(io::stdin()),
+        _ => Box::new(fs::File::open(input).unwrap())
     };
-    io::copy(&mut reader, &mut io::stdout()).unwrap();
+
+    //io::copy(&mut reader, &mut io::stdout()).unwrap();
+
+    let mut buffer = String::new();
+    let count = match reader.read_to_string(&mut buffer) {
+        Ok(n) => n,
+        Err(_) => 0
+    };
+
+    println!("({}) {}", count, buffer); 
 }
 
